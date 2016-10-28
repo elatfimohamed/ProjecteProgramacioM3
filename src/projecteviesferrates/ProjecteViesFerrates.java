5bc9557cece39b79a2253e4280066cae0e63ea66
alumne <alumne@mohamedelatfi-iesebre>
2016-10-28 09:33:53 +0200

    new commit

--- src/projecteviesferrates/ProjecteViesFerrates.java
+++ src/projecteviesferrates/ProjecteViesFerrates.java
@@ -18,33 +18,63 @@ public class ProjecteViesFerrates {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         boolean material = false, casco = false, arnes = false, tercebaga = false, buitcorda = false, guants = false, calsat = false;
+        boolean material = false, casco = false, arnes = false, tercebaga = false, buitcorda = false, guants = false, calsat = false;
         String nomvia = null, localitat = null, regio = null, provincia = null;
+        
+        boolean dificultat = false;
         int desnivel = 0;
         float longitut = 0;
-        char dificultat = ' ';
-
+        char esdificultat = ' ';
+        char esmaterial = ' ';
         Scanner entrada = new Scanner(System.in);
+        Scanner entNum=new Scanner(System.in);
+        Scanner entText=new Scanner(System.in);
+        int i;
+        String text;
+
+       
 
         int opcio;         // l'opció introduïda per l'usuari
 
         //Bucle per mostra el menú
         do {
-            System.out.println("<------------------------>");
-            System.out.println("\nMENU");
-            System.out.println("0.Sortir del programa");
-            System.out.println("1.Introdueix una via");
-            System.out.println("2. Borrar Via ");
-            System.out.println("3. Modificar via ");
-            System.out.println("4. Llistar Vies");
-            System.out.println("");
-            System.out.println("Tria una opció:");
-            System.out.println("<------------------------>");
+
+            System.out.println("+------------------------ MENU ------------------------+");
+            System.out.println("| 0. Sortir del programa.                              |");
+            System.out.println("| 1. Introdueix una via.                               |");
+            System.out.println("| 2. Borrar Via                                        |");
+            System.out.println("| 3. Modificar Via                                     |");
+            System.out.println("| 4. Llistar Vies                                      |");
+            System.out.println("+------------------------------------------------------+");
+            System.out.print("--> Tria una opció: ");
 
             switch (opcio = entrada.nextInt()) {
 
                 case 1:
-                    System.out.println("Introdueix una via:");
+                    System.out.println("Introdueix una via:");   
+                     text = entText.nextLine();
+                    nomvia = entrada.nextLine();
+                    System.out.println("Introdueix localitat");
+                    localitat = entrada.nextLine();
+                    System.out.println("Introdueix regio");
+                    regio = entrada.nextLine();
+                    System.out.println("Introdueix provincia");
+                    provincia = entrada.nextLine();
+                    System.out.println("És dificil o no ? (S/N):");
+                    do {
+                        esdificultat = entrada.nextLine().toUpperCase().charAt(0);
+
+                    } while (esdificultat != 'S' && esdificultat != 'N');
+                    dificultat = (esdificultat == 'S');
+                    
+                    System.out.println("És necesari el us del material? (S/N)");
+                    do {
+                        esmaterial = entrada.nextLine().toUpperCase().charAt(0);
+
+                    } while (esmaterial != 'S' && esmaterial != 'N');
+                    material = (esmaterial == 'S');
+                    
+                    
 
                     break;
                 case 2:
@@ -73,19 +103,3 @@ public class ProjecteViesFerrates {
     }
 
 }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-   
